a52c49d3d948b3da6d2abe68e08898de5380a420
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2020-06-15 13:35:46 +0700

    Update createbootableusb.sh

--- createbootableusb.sh
+++ createbootableusb.sh
@@ -2,28 +2,17 @@
 fdisk -l
 echo "Set your usb drive"
 read drive
-cfdisk $drive
-fdisk -l
-echo "What is your first partition?"
-read drive1
-fdisk -l
-echo "What is your second partition?"
-read drive2
-mkfs.vfat $drive1
-mkfs.exfat $drive2
+dd if=/dev/zero of=$drive
+parted $drive mklable gpt
+parted $drive mkpart ext4 ext4 1 6001 
 mkdir part1
-mkdir part2
 mkdir iso
 mount Win.iso iso
 cp -r iso/*  WINDOWS\(exfat\)/
 umount iso
 rm -rf iso
-mount $drive1 part1
-cp  -r UEFI_DRIVERS\(vfat\)/*  part1
-umount part1
-rm -rf part1
-mount $drive2 part2
-cp -r WINDOWS\(exfat\)/*   part2
+mount $drive2 part1
+cp -r WINDOWS\(exfat\)/*   part1
 umount part2
 rm -rf part2
 
